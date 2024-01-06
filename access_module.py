6a75896e9342003719e890882baf19fd120fabb8
Hokyung (Andy) Lee <techandy42@gmail.com>
2024-01-06 14:31:37 -0500

    converted categorical classifications into regressive classification

--- access_module.py
+++ access_module.py
@@ -1,26 +1,32 @@
 from gpt_module import clean_response, gpt_call
 from pydantic import BaseModel
-from product_module import get_product
-from typing import Literal
-
-Levels = Literal["Easy", "Medium", "Hard", "easy", "medium", "hard"]
 
 class Response(BaseModel):
-     access: Levels
+     access_level: float
 
 json_format = """
 {
-  "access_level": "Easy" or "Medium" or "Hard"
+  "access_level": float
 }
 """
 
-def get_access(problem, solution):
-  product = get_product(problem, solution)
+def get_access(problem, solution, product):
   sys_msg = None
   business_problem = f""" {problem} """
   business_solution = f""" {solution} """
   prompt = f"""
-You will be presented with a product name in a circular economy business. Given the name of the product and based on the following evaluation criteria, assess how difficult it is to gain access to the product. Use a scale of 'Easy', 'Medium', and 'Hard' to rate the difficulty, where 'Easy' implies a simpler retrieval process and 'Hard' implies a more complex and difficult retrieval process. Evaluation criteria: 1) Public participation: it is easier to retrieve a product if the public is enthusiastic to return or provide such product (EX: the public is willing to return plastic bottles in Norway (Easy)) 2) Infrastructure and accessibility: Straightforward and streamlined retrieval processes makes is easier to access the product (EX: recycling bins (Easy), washing machines (Hard), industrial equipment requiring infrastructural partnerships to retrieve (Hard), products located in remote or difficult-to-reach areas(Hard), wind turbines (Hard), products requiring specialized equipment or expertise to retrieve (Hard)) 3) Existence of secondary markets: products containing materials with higher resale value are harder to access (EX: construction equipment (Hard), carpets (Easy)).
+You will be presented with a product name, business problem, and business solution for a business idea in the circular economy. 
+Based on the following evaluation criteria, assess how difficult it is to gain access to the product. 
+Use a scale between 0 to 1 to rate the difficulty, where closer to 0 (easy) implies a simpler retrieval process and closer to 1 (hard) implies a more complex and difficult retrieval process. 
+Evaluation criteria: 
+1) Public participation: it is easier to retrieve a product if the public is enthusiastic to return or provide such product 
+(EX: if the product is water bottle recycling in Norway, and if Norwegian citizens are more willing to recycle water bottles, then the score will lean closer to 0) 
+2) Infrastructure and accessibility: Straightforward and streamlined retrieval processes makes is easier to access the product 
+(EX: recycling bins (easy, closer to 0), washing machines (hard, closer to 1), industrial equipment requiring infrastructural partnerships to retrieve (hard, closer to 1), products located in remote or difficult-to-reach areas (hard, closer to 1), wind turbines (hard, closer to 1), products requiring specialized equipment or expertise to retrieve (hard, closer to 1)) 3) Existence of secondary markets: products containing materials with higher resale value are harder to access (EX: construction equipment (hard, closer to 1), carpets (easy, closer to 0)).
+
+Product Name:
+
+{product}
 
 Business Problem:
 
@@ -34,18 +40,24 @@ Business Solution:
 
   cleaned_response = clean_response(response)
 
-#   print(cleaned_response)
+  print(cleaned_response)
   
   validated_response = Response.model_validate_json(cleaned_response)
 
-#   print(validated_response)
+  print(validated_response)
   
   return validated_response
    
 
 # Test Cases
 if __name__ == "__main__":
-   get_access("The construction industry is indubitably one of the significant contributors to global waste, contributing approximately 1.3 billion tons of waste annually, exerting significant pressure on our landfills and natural resources. Traditional construction methods entail single-use designs that require frequent demolitions, leading to resource depletion and wastage."
-               ,"Herein, we propose an innovative approach to mitigate this problem: Modular Construction. This method embraces recycling and reuse, taking a significant stride towards a circular economy.   Modular construction involves utilizing engineered components in a manufacturing facility that are later assembled on-site. These components are designed for easy disassembling, enabling them to be reused in diverse projects, thus significantly reducing waste and conserving resources.  Not only does this method decrease construction waste by up to 90%, but it also decreases construction time by 30-50%, optimizing both environmental and financial efficiency. This reduction in time corresponds to substantial financial savings for businesses. Moreover, the modular approach allows greater flexibility, adapting to changing needs over time.  We believe, by adopting modular construction, the industry can transit from a 'take, make and dispose' model to a more sustainable 'reduce, reuse, and recycle' model, driving the industry towards a more circular and sustainable future. The feasibility of this concept is already being proven in markets around the globe, indicating its potential for scalability and real-world application.")
-   get_access("The massive shift in student learning towards digital platforms has resulted in an increased carbon footprint due to energy consumption from data centers and e-waste from obsolete devices. Simultaneously, physical books are often produced, used once, and then discarded, leading to waste and deforestation.",
-               "Implement a ""Book Swap"" program within educational institutions and local communities. This platform allows students to trade books they no longer need with others who require them, reducing the need for new book production and hence, lowering the rate of resource depletion. Furthermore, the platform could have a digital component to track book exchanges, giving users credits for each trade, which they can accrue and redeem. This system encourages and amplifies the benefits of reusing and sharing resources, thus contributing to the circular economy.   By integrating gamification, getting students and parents involved and providing an easy-to-use platform, the program could influence a cultural shift towards greater resource value appreciation and waste reduction. In terms of the financial aspect, less reliance on purchasing new books could save money for students, parents and schools.")
+    get_access(
+      problem="The construction industry is indubitably one of the significant contributors to global waste, contributing approximately 1.3 billion tons of waste annually, exerting significant pressure on our landfills and natural resources. Traditional construction methods entail single-use designs that require frequent demolitions, leading to resource depletion and wastage.",
+      solution="Herein, we propose an innovative approach to mitigate this problem: Modular Construction. This method embraces recycling and reuse, taking a significant stride towards a circular economy.   Modular construction involves utilizing engineered components in a manufacturing facility that are later assembled on-site. These components are designed for easy disassembling, enabling them to be reused in diverse projects, thus significantly reducing waste and conserving resources.  Not only does this method decrease construction waste by up to 90%, but it also decreases construction time by 30-50%, optimizing both environmental and financial efficiency. This reduction in time corresponds to substantial financial savings for businesses. Moreover, the modular approach allows greater flexibility, adapting to changing needs over time.  We believe, by adopting modular construction, the industry can transit from a 'take, make and dispose' model to a more sustainable 'reduce, reuse, and recycle' model, driving the industry towards a more circular and sustainable future. The feasibility of this concept is already being proven in markets around the globe, indicating its potential for scalability and real-world application.",
+      product="Modular Construction"
+    )
+    get_access(
+      problem="The massive shift in student learning towards digital platforms has resulted in an increased carbon footprint due to energy consumption from data centers and e-waste from obsolete devices. Simultaneously, physical books are often produced, used once, and then discarded, leading to waste and deforestation.",
+      solution="Implement a ""Book Swap"" program within educational institutions and local communities. This platform allows students to trade books they no longer need with others who require them, reducing the need for new book production and hence, lowering the rate of resource depletion. Furthermore, the platform could have a digital component to track book exchanges, giving users credits for each trade, which they can accrue and redeem. This system encourages and amplifies the benefits of reusing and sharing resources, thus contributing to the circular economy.   By integrating gamification, getting students and parents involved and providing an easy-to-use platform, the program could influence a cultural shift towards greater resource value appreciation and waste reduction. In terms of the financial aspect, less reliance on purchasing new books could save money for students, parents and schools.",
+      product="Book Swap"  
+    )
--- processing_module.py
+++ processing_module.py
@@ -1,25 +1,30 @@
 from gpt_module import clean_response, gpt_call
 from pydantic import BaseModel
-from typing import Literal
-
-Levels = Literal["Easy", "Medium", "Hard", "easy", "medium", "hard"]
-
 class Response(BaseModel):
-     process: Levels
+     processing_level: float
 
 json_format = """
 {
-  "process_level": "Easy" or "Medium" or "Hard"
+  "processing_level": float
 }
 """
 
-def get_processing(problem, solution):
+def get_processing(problem, solution, product):
   sys_msg = None
   business_problem = f""" {problem} """
   business_solution = f""" {solution} """
   prompt = f"""
-You will be presented with a problem in the circular economy and a corresponding business solution. Given the description and based on the following evaluation criteria, assess the level of processing difficulty for the business solution. Use a scale of 'Easy', 'Medium', and 'Hard' to rate the difficulty, where 'Easy' implies a simpler breakdown process and 'Hard' implies a more complex industrial process.
-Evaluation Criteria: 1) Size: Assess the difficulty of processing based on the mass of the product. (EX: extremely heavy or bulky products (Hard), light and small products (Easy), washing machine (Hard), ink cartridge (Easy)) 2) Chemical Toxicity: more hazardous materials are more difficult and expensive to process (EX: Non-toxic materials like paper (Easy), Hazardous chemicals or radioactive materials (Hard), advanced smartphones and laptops (Hard)) 3) Technology Needed: Rate the processing difficulty based on the level of technology required for processing (EX: Low-tech processing like manual labor (Easy), High-tech processes involving specialized machinery (Hard))
+You will be presented with a product name, business problem, and business solution for a business idea in the circular economy.
+Based on the following evaluation criteria, assess the level of processing difficulty for the business solution. 
+Use a scale of 0-1 to rate the difficulty, where closer to 0 (easy) implies a simpler breakdown process and closer to 1 (hard) implies a more complex industrial process.
+Evaluation Criteria: 
+1) Size: Assess the difficulty of processing based on the mass of the product. (EX: extremely heavy or bulky products (hard, closer to 1), light and small products (easy, closer to 0), washing machine (hard, closer to 1), ink cartridge (easy, closer to 0)) 
+2) Chemical Toxicity: more hazardous materials are more difficult and expensive to process (EX: Non-toxic materials like paper (easy, closer to 0), Hazardous chemicals or radioactive materials (hard, closer to 1), advanced smartphones and laptops (hard, closer to 1)) 
+3) Technology Needed: Rate the processing difficulty based on the level of technology required for processing (EX: Low-tech processing like manual labor (easy, closer to 0), High-tech processes involving specialized machinery (hard, closer to 1))
+
+Product Name:
+
+{product}
 
 Business Problem:
 
@@ -33,18 +38,24 @@ Business Solution:
 
   cleaned_response = clean_response(response)
 
-#   print(cleaned_response)
+  print(cleaned_response)
   
   validated_response = Response.model_validate_json(cleaned_response)
 
-#   print(validated_response)
+  print(validated_response)
   
   return validated_response
    
 
 # Test Cases
 if __name__ == "__main__":
-   get_processing("The construction industry is indubitably one of the significant contributors to global waste, contributing approximately 1.3 billion tons of waste annually, exerting significant pressure on our landfills and natural resources. Traditional construction methods entail single-use designs that require frequent demolitions, leading to resource depletion and wastage."
-               ,"Herein, we propose an innovative approach to mitigate this problem: Modular Construction. This method embraces recycling and reuse, taking a significant stride towards a circular economy.   Modular construction involves utilizing engineered components in a manufacturing facility that are later assembled on-site. These components are designed for easy disassembling, enabling them to be reused in diverse projects, thus significantly reducing waste and conserving resources.  Not only does this method decrease construction waste by up to 90%, but it also decreases construction time by 30-50%, optimizing both environmental and financial efficiency. This reduction in time corresponds to substantial financial savings for businesses. Moreover, the modular approach allows greater flexibility, adapting to changing needs over time.  We believe, by adopting modular construction, the industry can transit from a 'take, make and dispose' model to a more sustainable 'reduce, reuse, and recycle' model, driving the industry towards a more circular and sustainable future. The feasibility of this concept is already being proven in markets around the globe, indicating its potential for scalability and real-world application.")
-   get_processing("The massive shift in student learning towards digital platforms has resulted in an increased carbon footprint due to energy consumption from data centers and e-waste from obsolete devices. Simultaneously, physical books are often produced, used once, and then discarded, leading to waste and deforestation.",
-               "Implement a ""Book Swap"" program within educational institutions and local communities. This platform allows students to trade books they no longer need with others who require them, reducing the need for new book production and hence, lowering the rate of resource depletion. Furthermore, the platform could have a digital component to track book exchanges, giving users credits for each trade, which they can accrue and redeem. This system encourages and amplifies the benefits of reusing and sharing resources, thus contributing to the circular economy.   By integrating gamification, getting students and parents involved and providing an easy-to-use platform, the program could influence a cultural shift towards greater resource value appreciation and waste reduction. In terms of the financial aspect, less reliance on purchasing new books could save money for students, parents and schools.")
+    get_processing(
+      problem="The construction industry is indubitably one of the significant contributors to global waste, contributing approximately 1.3 billion tons of waste annually, exerting significant pressure on our landfills and natural resources. Traditional construction methods entail single-use designs that require frequent demolitions, leading to resource depletion and wastage.",
+      solution="Herein, we propose an innovative approach to mitigate this problem: Modular Construction. This method embraces recycling and reuse, taking a significant stride towards a circular economy.   Modular construction involves utilizing engineered components in a manufacturing facility that are later assembled on-site. These components are designed for easy disassembling, enabling them to be reused in diverse projects, thus significantly reducing waste and conserving resources.  Not only does this method decrease construction waste by up to 90%, but it also decreases construction time by 30-50%, optimizing both environmental and financial efficiency. This reduction in time corresponds to substantial financial savings for businesses. Moreover, the modular approach allows greater flexibility, adapting to changing needs over time.  We believe, by adopting modular construction, the industry can transit from a 'take, make and dispose' model to a more sustainable 'reduce, reuse, and recycle' model, driving the industry towards a more circular and sustainable future. The feasibility of this concept is already being proven in markets around the globe, indicating its potential for scalability and real-world application.",
+      product="Modular Construction"
+    )
+    get_processing(
+      problem="The massive shift in student learning towards digital platforms has resulted in an increased carbon footprint due to energy consumption from data centers and e-waste from obsolete devices. Simultaneously, physical books are often produced, used once, and then discarded, leading to waste and deforestation.",
+      solution="Implement a ""Book Swap"" program within educational institutions and local communities. This platform allows students to trade books they no longer need with others who require them, reducing the need for new book production and hence, lowering the rate of resource depletion. Furthermore, the platform could have a digital component to track book exchanges, giving users credits for each trade, which they can accrue and redeem. This system encourages and amplifies the benefits of reusing and sharing resources, thus contributing to the circular economy.   By integrating gamification, getting students and parents involved and providing an easy-to-use platform, the program could influence a cultural shift towards greater resource value appreciation and waste reduction. In terms of the financial aspect, less reliance on purchasing new books could save money for students, parents and schools.",
+      product="Book Swap"  
+    )
