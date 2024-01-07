9907a9ae13f8083d0f63674b6c07729794d474a5
Serena Pei <serenapei@Serenas-MacBook-Pro.local>
2024-01-06 23:56:00 -0800

    solution rating numerical data

---
+++ solution_rating.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import openai, numpy as np
+from openai.embeddings_utils import get_embedding, cosine_similarity
+
+api_key = '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'
+openai.api_key = api_key
+
+df = pd.read_csv('./outputs/combined_data_first_200_rows.csv', encoding='latin-1')
+solutions = list(df['solution'])
+embedded_values = list(df['embedded_value'])
+access_levels = list(df['access_level'])
+processing_levels = list(df['processing_level'])
+
+# modifies score_tracker according to data, regular represents positive correlation
+def scorer(score_tracker, data, regular):
+    if regular:
+        for idx in range(len(solutions)):
+            var = data[idx]
+            if var <= 0.3:
+                score_tracker[idx+1] = 1
+            elif var <= 0.6:
+                score_tracker[idx+1] = 2
+            elif var <= 0.9:
+                score_tracker[idx+1] = 3
+            elif var <= 1.2:
+                score_tracker[idx+1] = 4
+            elif var <= 1.5:
+                score_tracker[idx+1] = 5
+            elif var <= 1.8:
+                score_tracker[idx+1] = 6
+            elif var <= 2.1:
+                score_tracker[idx+1] = 7
+            elif var <= 2.4:
+                score_tracker[idx+1] = 8
+            elif var <= 2.7:
+                score_tracker[idx+1] = 9
+            elif var <= 3.0:
+                score_tracker[idx+1] = 10
+    else:
+        for idx in range(len(solutions)):
+            var = data[idx]
+            if var <= 0.3:
+                score_tracker[idx+1] = 10
+            elif var <= 0.6:
+                score_tracker[idx+1] = 9
+            elif var <= 0.9:
+                score_tracker[idx+1] = 8
+            elif var <= 1.2:
+                score_tracker[idx+1] = 7
+            elif var <= 1.5:
+                score_tracker[idx+1] = 6
+            elif var <= 1.8:
+                score_tracker[idx+1] = 5
+            elif var <= 2.1:
+                score_tracker[idx+1] = 4
+            elif var <= 2.4:
+                score_tracker[idx+1] = 3
+            elif var <= 2.7:
+                score_tracker[idx+1] = 2
+            elif var <= 3.0:
+                score_tracker[idx+1] = 1
+
+# scores based on each factor
+scores_em = {} # higher embedded value -> higher score
+scores_ac = {} # higher access level -> lower score
+scores_pr = {} # higher processing level -> lower score
+
+scorer(scores_em, embedded_values, True)
+scorer(scores_ac, access_levels, False)
+scorer(scores_pr, processing_levels, False)
+
+# make new dict with avg scores from each scores dict above
+avg_scores = {}
+for idx in range(len(solutions)):
+    avg_scores[idx+1] = sum(scores_em[idx+1], scores_ac[idx+1], scores_pr[idx+1]) / 3
+
+# gives an overall score based on the average of individual scores
+scores_overall = {}
+scorer(scores_overall, avg_scores, True)
