457d4e091fba8be6ac2fea66e555d59c8d11963f
malindalu <malindalu0611@gmail.com>
2024-01-06 21:02:08 -0500

    embedding clustering

--- clustering_test.py
+++ clustering_test.py
@@ -2,9 +2,59 @@ import pandas as pd
 import openai, numpy as np
 from openai.embeddings_utils import get_embedding, cosine_similarity
 
-api_key = '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'
+api_key = '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'
 openai.api_key = api_key
 
-resp = openai.Embedding.create(
-    input=["eating food", "I am hungry", "I am traveling" , "exploring new places"],
-    engine="text-similarity-davinci-001")
+# resp = openai.Embedding.create(
+#     input=["eating food", "I am hungry", "I am traveling" , "exploring new places"],
+#     engine="text-similarity-davinci-001")
+# resp['data'][0].keys()
+
+# source: https://stackoverflow.com/questions/55619176/how-to-cluster-similar-sentences-using-bert
+from sklearn.cluster import KMeans
+
+df = pd.read_csv('AI_EarthHack_Dataset.csv', encoding='latin-1')
+solutions = list(df['solution'])
+print(solutions[0])
+
+
+# # Corpus with example sentences
+# corpus = ['A man is eating food.',
+#           'A man is eating a piece of bread.',
+#           'Horse is eating grass.',
+#           'A man is eating pasta.',
+#           'A Woman is eating Biryani.',
+#           'The girl is carrying a baby.',
+#           'The baby is carried by the woman',
+#           'A man is riding a horse.',
+#           'A man is riding a white horse on an enclosed ground.',
+#           'A monkey is playing drums.',
+#           'Someone in a gorilla costume is playing a set of drums.',
+#           'A cheetah is running behind its prey.',
+#           'A cheetah chases prey on across a field.',
+#           'The cheetah is chasing a man who is riding the horse.',
+#           'man and women with their baby are watching cheetah in zoo'
+#           ]
+response = openai.Embedding.create(
+    input=solutions[:800],
+    model="text-similarity-babbage-001"
+)
+print(type(response['data']))
+
+solutions_embeddings = [ d['embedding'] for d in response['data']]
+# # Normalize the embeddings to unit length
+solutions_embeddings = solutions_embeddings /  np.linalg.norm(solutions_embeddings, axis=1, keepdims=True)
+clustering_model = KMeans(n_clusters=8)
+clustering_model.fit(solutions_embeddings)
+cluster_assignment = clustering_model.labels_
+print(cluster_assignment)
+
+clustered_sentences = {}
+for sentence_id, cluster_id in enumerate(cluster_assignment):
+    if cluster_id not in clustered_sentences:
+        clustered_sentences[cluster_id] = []
+    clustered_sentences[cluster_id].append(solutions[sentence_id])
+print(clustered_sentences[1][:3])
+    
+
+
