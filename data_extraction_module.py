3b00dddc8c33a6d03024403813228a6d7403566f
Hokyung (Andy) Lee <techandy42@gmail.com>
2024-01-14 16:14:02 -0500

    returns added index list

--- data_extraction_module.py
+++ data_extraction_module.py
@@ -23,13 +23,16 @@ def get_last_index(jsonl_file_name):
       return last_json_object['id']
 
 def extract_data_from_csv_file(df, jsonl_file_name):
+  added_index_list = []
   with open(jsonl_file_name, 'a') as file:
     for index, row in df.iterrows():
       try:
         product, summary, embedded_value, embedded_value_reasoning, access_level, access_level_reasoning, processing_level, processing_level_reasoning, categories, categories_reasoning = ask_questions_in_tree(row['problem'], row['solution'])
         last_index = get_last_index(jsonl_file_name)
+        current_index = last_index + 1 + index
+        added_index_list.append(current_index)
         data = {
-          "id": last_index + 1,
+          "id": current_index,
           "product": product,
           "summary": summary,
           "embedded_value": embedded_value,
@@ -53,7 +56,11 @@ def extract_data_from_csv_file(df, jsonl_file_name):
         print("=" * 50)
         continue
 
+  return added_index_list
+
 if __name__ == "__main__":
   df = pd.read_csv('AI_EarthHack_Dataset.csv', encoding='latin-1')
-  df = df.iloc[:1]
-  extract_data_from_csv_file(df, "./outputs/combined_data_first_200_rows.jsonl")
+  df = df.iloc[:5]
+  added_index_list = extract_data_from_csv_file(df, "./outputs/extracted_data_training_dataset.jsonl")
+  print("added_index_list:")
+  print(added_index_list)
